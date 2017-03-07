3f9cf3526eed813a3556a2f73365cb4d1a52f7e9
helei112g <helei5200@126.com>
2017-03-07 23:22:17 +0800

    chore:发布3.0.0版本

--- src/Config.php
+++ src/Config.php
@@ -13,7 +13,7 @@ namespace Payment;
 
 final class Config
 {
-    const VERSION = 'dev-payment3';
+    const VERSION = '3.0.0';
 
     //========================= ali相关接口 =======================//
     // 支付相关常量
