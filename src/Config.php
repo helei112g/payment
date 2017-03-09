b5de290d4d9d2578b126cd74818585b5adf60a6b
helei112g <helei5200@126.com>
2017-03-09 16:20:30 +0800

    fix:标记版本

--- src/Config.php
+++ src/Config.php
@@ -13,7 +13,7 @@ namespace Payment;
 
 final class Config
 {
-    const VERSION = 'dev-master';
+    const VERSION = '3.0.2';
 
     //========================= ali相关接口 =======================//
     // 支付相关常量
