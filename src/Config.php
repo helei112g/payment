6ddd8c1a1e7fbba704c953e814fea5cf568bb288
helei <helei5200@126.com>
2017-07-11 10:57:36 +0800

    结构图描述

--- src/Config.php
+++ src/Config.php
@@ -13,7 +13,7 @@ namespace Payment;
 
 final class Config
 {
-    const VERSION = '3.1.1-dev';
+    const VERSION = '4.0.2-dev';
 
     //========================= ali相关接口 =======================//
     // 支付相关常量
