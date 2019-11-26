8b4b7e3b8b1bea9bff00d1a619e03a6335965de5
dayu <dayugog@gmail.com>
2019-11-26 21:41:32 +0800

    feat: add trade create function for alipay

--- src/Gateways/Alipay/TradeBankCreate.php
+++ src/Gateways/Alipay/TradeBankCreate.php
@@ -20,13 +20,13 @@ use Payment\Payment;
  * @package Payment\Gateways\Alipay
  * @author  : Leo
  * @email   : dayugog@gmail.com
- * @date    : 2019/3/31 9:12 AM
+ * @date    : 2019/11/26 9:36 PM
  * @version : 1.0.0
- * @desc    : 统一收单交易退款查询
+ * @desc    : 网商银行全渠道收单业务订单创建
  **/
-class RefundQuery extends AliBaseObject implements IGatewayRequest
+class TradeBankCreate extends AliBaseObject implements IGatewayRequest
 {
-    const METHOD = 'alipay.trade.fastpay.refund.query';
+    const METHOD = 'mybank.payment.trade.order.create';
 
     /**
      * @param array $requestParams
@@ -35,10 +35,18 @@ class RefundQuery extends AliBaseObject implements IGatewayRequest
     protected function getBizContent(array $requestParams)
     {
         $bizContent = [
-            'out_trade_no'   => $requestParams['out_trade_no'] ?? '',
-            'trade_no'       => $requestParams['trade_no'] ?? '',
-            'out_request_no' => $requestParams['refund_no'] ?? '',
-            'org_pid'        => $requestParams['org_pid'] ?? '',
+            'partner_id'       => $requestParams['partner_id'] ?? '',
+            'out_trade_no'     => $requestParams['out_trade_no'] ?? '',
+            'recon_related_no' => $requestParams['recon_related_no'] ?? '',
+            'pd_code'          => $requestParams['pd_code'] ?? '',
+            'ev_code'          => $requestParams['ev_code'] ?? '',
+            'total_amount'     => $requestParams['total_amount'] ?? '',
+            'currency_code'    => $requestParams['currency_code'] ?? '',
+            'goods_info'       => $requestParams['goods_info'] ?? '',
+            'seller_id'        => $requestParams['seller_id'] ?? '',
+            'pay_type'         => $requestParams['pay_type'] ?? '',
+            'pay_date'         => $requestParams['pay_date'] ?? '',
+            'remark'           => $requestParams['remark'] ?? '',
         ];
         $bizContent = ArrayUtil::paraFilter($bizContent);
 
@@ -58,10 +66,10 @@ class RefundQuery extends AliBaseObject implements IGatewayRequest
             $ret    = $this->post($this->gatewayUrl, $params);
             $retArr = json_decode($ret, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new GatewayException(sprintf('format refund data get error, [%s]', json_last_error_msg()), Payment::FORMAT_DATA_ERR, ['raw' => $ret]);
+                throw new GatewayException(sprintf('format trade bank create get error, [%s]', json_last_error_msg()), Payment::FORMAT_DATA_ERR, ['raw' => $ret]);
             }
 
-            $content = $retArr['alipay_trade_fastpay_refund_query_response'];
+            $content = $retArr['mybank_payment_trade_order_create_response'];
             if ($content['code'] !== self::REQ_SUC) {
                 throw new GatewayException(sprintf('request get failed, msg[%s], sub_msg[%s]', $content['msg'], $content['sub_msg']), Payment::SIGN_ERR, $content);
             }
